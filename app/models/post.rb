0629a50c2280bc153c57932869e4f2336819e746
Hi-a-m <riverdale.kampw@gmail.com>
2023-01-08 00:13:35 +0900

    Rails再提出2回目

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ApplicationRecord
     validate :start_end_check
       def start_end_check
         errors.add(:end_at, "の日付を正しく記入してください。") unless
-        self.start_at < self.end_at
+        self.start_at <= self.end_at
       end
+    
+
 end
